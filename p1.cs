4783ebcdb4cf899c005f3f0befbddc2d5b4102a9
sanskriti27173 <sanskritityagi68@gmail.com>
2025-02-09 20:01:27 -0800

    Update p1.cs

--- p1.cs
+++ p1.cs
@@ -7,3 +7,20 @@ class Program
   console.WriteLine("C# SDK is worling correctly."):
 }
 }
+
+
+
+using System;
+
+class DataTypesExample 
+{
+  static void Main()
+  {
+    int intValue = 10;
+    float floatValue = 10.5f;
+    double doubleValue = 20.99;
+    char charValue = 'A';
+    bool boolValue = 'true';
+    string stringValue = "HELLO,C#";
+
+    Console.WriteLine("Integer
